bfddedf3e474f72d8f3737bedb43e0e46c6375c2
Anders Schau Knatten <anders@knatten.org>
2013-04-13 17:23:34 +0200

    Added about and help

--- cppquiz/quiz/urls.py
+++ cppquiz/quiz/urls.py
@@ -1,9 +1,13 @@
 from django.conf.urls.defaults import *
+from django.views.generic.simple import direct_to_template
+
 
 from quiz import views
 
 urlpatterns = patterns('',
     url(r'^quiz/question/(?P<question_id>.*)', views.question, name='question'),
     url(r'^quiz/clear', views.clear, name='clear'),
+    url(r'^quiz/about', direct_to_template, {'template': 'quiz/help.html'}),
+    url(r'^quiz/help', direct_to_template, {'template': 'quiz/help.html'}),
     url(r'^$', views.index, name='index'),
 )
