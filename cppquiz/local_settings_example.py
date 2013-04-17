4f68df2f58f0c3c4ad5b85f8299d72ddf9a8c90f
Anders Schau Knatten <anders@knatten.org>
2013-04-17 17:42:25 +0200

    Improved example for local_settings

--- cppquiz/local_settings_example.py
+++ cppquiz/local_settings_example.py
@@ -3,7 +3,7 @@ import settings
 settings.DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'NAME': '/path/to/your/code/cppquiz/db.sqlite3',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -12,5 +12,8 @@ settings.DATABASES = {
 }
 
 settings.TEMPLATE_DIRS = (
-    '',
+    '/path/to/your/code/cppquiz/templates',
 )
+
+settings.STATIC_ROOT=''
+settings.STATIC_URL='/static/'
