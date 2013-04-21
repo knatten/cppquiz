2de34eb66b8d64ae162799274aa8e41d196564c2
Anders Schau Knatten <anders@knatten.org>
2013-04-21 11:32:13 +0200

    Add favicon and robots.txt

--- cppquiz/urls.py
+++ cppquiz/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.views.generic.simple import direct_to_template
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -15,4 +16,5 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
     url(r'', include('quiz.urls')),
+    (r'^robots\.txt$', direct_to_template, {'template': 'robots.txt', 'mimetype': 'text/plain'}),
 )
