663d2bebd51db7bf4ef51e69b9ea65d2de0a6f3c
Anders Schau Knatten <anders@knatten.org>
2013-05-01 06:48:31 +0200

    Questions can be tagged with a difficulty level

--- cppquiz/quiz/models.py
+++ cppquiz/quiz/models.py
@@ -1,14 +1,18 @@
 import datetime
 from django.db import models
 
-# Create your models here.
-
 class Question(models.Model):
     RESULT_CHOICES = (
         ('OK', 'is compilable and deterministic'),
         ('CE', 'has a compilation error'),
         ('US', 'is unspecified'),
         ('UD', 'is undefined'),
+    )
+    DIFFICULTY_CHOICES = (
+        (0, 'Not set'),
+        (1, 'Beginner'),
+        (2, 'Intermediate'),
+        (3, 'Expert'),
     )
     question = models.TextField(default='', blank=True)
     result = models.CharField(max_length=2, default='OK', choices=RESULT_CHOICES)
@@ -17,6 +21,7 @@ class Question(models.Model):
     date_time = models.DateTimeField(auto_now_add=True, default=datetime.datetime.now)
     published = models.BooleanField(default=True)
     author_email = models.EmailField(max_length=254, blank=True, default='')
+    difficulty = models.IntegerField(default=0, choices=DIFFICULTY_CHOICES)
 
 class UsersAnswer(models.Model):
     question = models.ForeignKey('Question')
