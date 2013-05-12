dd29b3d4af59670ea0778db6c2a872a3e4989fca
Anders Schau Knatten <anders@knatten.org>
2013-05-12 09:17:08 +0200

    Don't go 500 if the user tries to visit '/quiz/question/' (without a question id)

--- cppquiz/quiz/urls.py
+++ cppquiz/quiz/urls.py
@@ -5,7 +5,7 @@ from django.views.generic.simple import direct_to_template
 from quiz import views
 
 urlpatterns = patterns('',
-    url(r'^quiz/question/(?P<question_id>.*)', views.question, name='question'),
+    url(r'^quiz/question/(?P<question_id>.+)', views.question, name='question'),
     url(r'^quiz/clear', views.clear, name='clear'),
     url(r'^quiz/random', views.random_question, name='random'),
     url(r'^quiz/created', direct_to_template, {'template': 'quiz/created.html'}),
