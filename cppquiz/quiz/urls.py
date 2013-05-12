ed590e3f809b52981ce8ce7c862b3f293d69e41b
Anders Schau Knatten <anders@knatten.org>
2013-05-12 21:21:46 +0200

    Remove more user of deprecated function

--- cppquiz/quiz/urls.py
+++ cppquiz/quiz/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls.defaults import *
-from django.views.generic.simple import direct_to_template
+from django.views.generic.base import TemplateView
 
 
 from quiz import views
@@ -8,10 +8,10 @@ urlpatterns = patterns('',
     url(r'^quiz/question/(?P<question_id>.+)', views.question, name='question'),
     url(r'^quiz/clear', views.clear, name='clear'),
     url(r'^quiz/random', views.random_question, name='random'),
-    url(r'^quiz/created', direct_to_template, {'template': 'quiz/created.html'}),
+    url(r'^quiz/created', TemplateView.as_view(template_name = 'quiz/created.html')),
     url(r'^quiz/create', views.create, name='create'),
     url(r'^quiz/categorize', views.categorize, name='categorize'),
-    url(r'^quiz/about', direct_to_template, {'template': 'quiz/help.html'}),
-    url(r'^quiz/help', direct_to_template, {'template': 'quiz/help.html'}),
+    url(r'^quiz/about', TemplateView.as_view(template_name = 'quiz/help.html')),
+    url(r'^quiz/help', TemplateView.as_view(template_name = 'quiz/help.html')),
     url(r'^$', views.index, name='index'),
 )
