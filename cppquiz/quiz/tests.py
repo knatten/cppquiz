370b38b48c3955a3ebfd6c9f390cf442390d3d4f
Anders Schau Knatten <anders@knatten.org>
2013-12-07 14:19:38 +0100

    Include all unit tests in suite. Fixes #24

--- cppquiz/quiz/tests.py
+++ cppquiz/quiz/tests.py
@@ -4,6 +4,8 @@ from fixed_quiz_test import *
 from fixed_quiz_integration_test import *
 from test_active_quiz import ActiveQuizTest
 from test_quiz_in_progress import *
+from session_test import *
+from templatetags.quiz_extras_test import *
 
 def suite():
     return unittest.TestSuite([
@@ -12,5 +14,10 @@ def suite():
         unittest.TestLoader().loadTestsFromTestCase(ActiveQuizTest),
         unittest.TestLoader().loadTestsFromTestCase(QuizInProgressTest),
         unittest.TestLoader().loadTestsFromTestCase(FixedQuizIntegrationTest),
+        unittest.TestLoader().loadTestsFromTestCase(UserDataTest),
+        unittest.TestLoader().loadTestsFromTestCase(save_user_dataTest),
+        unittest.TestLoader().loadTestsFromTestCase(CodeTagsTest),
+        unittest.TestLoader().loadTestsFromTestCase(standard_ref_Test),
+        unittest.TestLoader().loadTestsFromTestCase(emphasize_test),
         ])
 
