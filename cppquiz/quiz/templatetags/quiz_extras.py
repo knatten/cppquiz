ca2fb573bf015f59c10509d70d9b1c69344c47bc
Anders Schau Knatten <anders@knatten.org>
2013-12-14 23:10:28 +0100

    Formatting of blocks of code based on indentation.
    Example:
        int main() {
            foo();
        }
    
    Closes #40

--- cppquiz/quiz/templatetags/quiz_extras.py
+++ cppquiz/quiz/templatetags/quiz_extras.py
@@ -15,6 +15,25 @@ def code_tags(text, autoescape=None):
     return mark_safe(
         re.sub('`(.*?)`', '<code>\\1</code>', text))
 
+@register.filter(needs_autoescape=True)
+def code_blocks(text, autoescape=None):
+    if autoescape:
+        text = conditional_escape(text)
+    new_text = ''
+    lines = text.splitlines(True)
+    in_code_block = False
+    for line in lines:
+        if not in_code_block and line.startswith('    '):
+            new_text += '</p><pre class="sh_cpp sh_sourceCode">'
+            in_code_block = True
+        if in_code_block and not line.startswith('    '):
+            new_text += '</pre><p>'
+            in_code_block = False
+        new_text += line
+    if in_code_block:
+        new_text += '</pre><p>'
+    return mark_safe(new_text)
+
 
 @register.filter(needs_autoescape=True)
 def standard_ref(text, autoescape=None):
