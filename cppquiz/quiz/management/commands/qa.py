7b24289364e6104f40c012cd7db80c82ec4ec526
Anders Schau Knatten <anders@knatten.org>
2014-03-07 20:46:00 +0100

    Add script to do simple QA (long lines, missing hints)

---
+++ cppquiz/quiz/management/commands/qa.py
@@ -0,0 +1,20 @@
+from django.core.management.base import BaseCommand, CommandError
+from cppquiz.quiz.models import *
+
+class Command(BaseCommand):
+#    args = '<poll_id poll_id ...>'
+#    help = 'Closes the specified poll for voting'
+    max_line_length = 60
+
+    def handle(self, *args, **options):
+        print "CHECKING FOR LONG LINES:"
+        for question in Question.objects.all():
+            for line in question.question.splitlines():
+                if len(line) > self.max_line_length:
+                    print "In question", question.pk, ":"
+                    print line
+        print
+        print "CHECKING FOR MISSING HINTS"
+        for question in Question.objects.all():
+            if question.published and len(question.hint) < 10:
+                print "Question", question.pk, "is published and missing a hint"
