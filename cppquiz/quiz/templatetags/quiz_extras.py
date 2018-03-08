ff7f2426f6bcb1e51cd2dd833e0fd8472bccbb4c
Anders Schau Knatten <anders@knatten.org>
2018-03-08 10:18:13 +0100

    Use markdown for formatting explanation and hint

--- cppquiz/quiz/templatetags/quiz_extras.py
+++ cppquiz/quiz/templatetags/quiz_extras.py
@@ -1,50 +1,24 @@
 # -*- coding: utf-8 -*-
 import re
+import markdown
 
 from django import template
 from django.utils.html import conditional_escape
 from django.utils.safestring import mark_safe
+from django.template import defaultfilters
 
 register = template.Library()
 
+def standard_ref(text):
+    return re.sub(u'(§[\d\.]+[¶\d]*)', '<em>\\1</em>', text)
+
+def custom_linebreaks(text):
+    return text.replace("\n", "<br />")
 
 @register.filter(needs_autoescape=True)
-def code_tags(text, autoescape=None):
-    if autoescape:
-        text = conditional_escape(text)
+def to_html(text, autoescape=None):
     return mark_safe(
-        re.sub('`(.*?)`', '<code>\\1</code>', text))
-
-@register.filter(needs_autoescape=True)
-def code_blocks(text, autoescape=None):
-    if autoescape:
-        text = conditional_escape(text)
-    new_text = ''
-    lines = text.splitlines(True)
-    in_code_block = False
-    for line in lines:
-        if not in_code_block and line.startswith('    '):
-            new_text += '</p><pre class="sh_cpp sh_sourceCode">'
-            in_code_block = True
-        if in_code_block and not line.startswith('    '):
-            new_text += '</pre><p>'
-            in_code_block = False
-        new_text += line
-    if in_code_block:
-        new_text += '</pre><p>'
-    return mark_safe(new_text)
-
-
-@register.filter(needs_autoescape=True)
-def standard_ref(text, autoescape=None):
-    if autoescape:
-        text = conditional_escape(text)
-    return mark_safe(
-        re.sub(u'(§[\d\.]+[¶\d]*)', '<em>\\1</em>', text))
-
-@register.filter(needs_autoescape=True)
-def emphasize(text, autoescape=None):
-    if autoescape:
-        text = conditional_escape(text)
-    return mark_safe(
-        re.sub(u'\*\*\*(.*)\*\*\*', '<strong>\\1</strong>', text))
+        standard_ref(
+            markdown.markdown(
+                custom_linebreaks(
+                    defaultfilters.urlize(text, False)))))
