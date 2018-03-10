e6227a5bebbc53d7cdc2309debbe65f068110bff
Anders Schau Knatten <anders@knatten.org>
2018-03-10 20:39:26 +0100

    Fix bug in markdown urls
    This broke in d100f11. Let's just handle all of this through markdown.
    When merging, remember to go though old-style urls and wrap them in `<>`
    to ensure markdown renders them as clickable urls.

--- cppquiz/quiz/templatetags/quiz_extras.py
+++ cppquiz/quiz/templatetags/quiz_extras.py
@@ -3,9 +3,7 @@ import re
 import markdown
 
 from django import template
-from django.utils.html import conditional_escape
 from django.utils.safestring import mark_safe
-from django.template import defaultfilters
 
 register = template.Library()
 
@@ -23,5 +21,4 @@ def to_html(text, autoescape=None):
     return mark_safe(
         standard_ref(
             custom_linebreaks(
-                markdown.markdown(
-                    defaultfilters.urlize(text, False)))))
+                markdown.markdown(text))))
