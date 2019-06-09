c7f37f52b6a41e30f84e738105bc69b8be898034
vsklamm <klammvs@gmail.com>
2019-06-09 04:47:29 +0300

    regex for links to standard fixes

--- cppquiz/quiz/templatetags/quiz_extras.py
+++ cppquiz/quiz/templatetags/quiz_extras.py
@@ -7,28 +7,30 @@ from django.utils.safestring import mark_safe
 
 register = template.Library()
 
-def format_paragraph(section_name, paragraph_number, full_text):
+def format_paragraph(section_name, paragraph_number, full_text, after_char):
+    if after_char:
+        full_text = full_text[:-1]
     if section_name:
         prefix = "https://timsong-cpp.github.io/cppwp/n4659/"
         full_link = prefix + section_name
         if paragraph_number:
             full_link = full_link + "#" + paragraph_number
-        return "<em><a href=\"" + full_link + "\">" + full_text + "</a></em>"
+        return "<em><a href=\"" + full_link + "\">" + full_text + "</a></em>" + after_char
     else:
-        return "<em>" + full_text + "</em>"
+        return "<em>" + full_text + "</em>" + after_char
 
 def standard_ref(text):
-    possible_named_reference = u'(\[(\S+(\.\S+)*)\])?'
+    possible_named_reference = u'(\[(\w+(\.\w+)*)\])?'
     numbered_reference = u'§\d+(\.\d+)*'
     possible_pilcrow_reference = u'(¶(\d+(\.\d+)*))*'
-    regex = re.compile('(' + possible_named_reference + '\(?' + numbered_reference + '\)?' + possible_pilcrow_reference + ')')
-    matched = regex.findall(text)
-    for elem in matched:
-        full_paragraph = elem[0]
-        section_name = elem[2]
-        paragraph_number = elem[6]
+    regex = re.compile('(' + possible_named_reference + '\(?' + numbered_reference + '\)?' + possible_pilcrow_reference + ')([^\.\d]?)')
+    for m in re.finditer(regex, text):        
+        full_paragraph = m.group(0)
+        section_name = m.group(3)
+        paragraph_number = m.group(7)
+        after_char = m.group(9)
         text = text.replace(full_paragraph,
-           format_paragraph(section_name, paragraph_number, full_paragraph))
+            format_paragraph(section_name, paragraph_number, full_paragraph, after_char))        
     return text
 
 def custom_linebreaks(text):
