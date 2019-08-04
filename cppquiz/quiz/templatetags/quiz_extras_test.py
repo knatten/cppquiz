f8c4ae4a43e416b0d82411769b33e926d0397c94
Anders Schau Knatten <anders@knatten.org>
2019-08-04 13:36:09 +0200

    Bugfix for links to the standard
    In ae2d127e0ec2, we added automatic links to the standard for all
    references. This didn't work in all cases though, as reported by @wrazik
    in #156.
    
    This commit fixes #156. Thanks to @vsklamm who added tests to
    reproduce the problem.

--- cppquiz/quiz/templatetags/quiz_extras_test.py
+++ cppquiz/quiz/templatetags/quiz_extras_test.py
@@ -19,23 +19,23 @@ class standard_ref_Test(unittest.TestCase):
 
     def test_given_name_references(self):
         self.assertEqual('<em><a href="https://timsong-cpp.github.io/cppwp/n4659/foo">[foo]\xa73.4</a></em>', standard_ref('[foo]§3.4'))
-        # self.assertEqual('<em><a href="https://timsong-cpp.github.io/cppwp/n4659/foo">[foo]\xa73.4</a></em>some_symbols', standard_ref('[foo]§3.4some_symbols'))
+        self.assertEqual('<em><a href="https://timsong-cpp.github.io/cppwp/n4659/foo">[foo]\xa73.4</a></em>some_symbols', standard_ref('[foo]§3.4some_symbols'))
         self.assertEqual('<em><a href="https://timsong-cpp.github.io/cppwp/n4659/foo.bar.baz#1">[foo.bar.baz]\xa73.4\xb61</a></em>', standard_ref('[foo.bar.baz]§3.4¶1'))
 
     def test_doesnt_include_too_much(self):
         self.assertEqual('<em>§3.4¶1</em>.', standard_ref('§3.4¶1.'))
-        # self.assertEqual('(<em>§3.4</em>)', standard_ref('(§3.4)'))
+        self.assertEqual('(<em>§3.4</em>)', standard_ref('(§3.4)'))
 
     def test_given_multiple_paragraphs(self):
         self.assertEqual('<em><a href="https://timsong-cpp.github.io/cppwp/n4659/foo">[foo]\xa73.4</a></em>, '
                          '<em><a href="https://timsong-cpp.github.io/cppwp/n4659/foo.bar.baz#1">[foo.bar.baz]\xa73.4\xb61</a></em>',
                           standard_ref('[foo]§3.4, [foo.bar.baz]§3.4¶1'))
-        # self.assertEqual('<em><a href="https://timsong-cpp.github.io/cppwp/n4659/foo.bar.baz#1">[foo.bar.baz]\xa73.4\xb61</a></em>, '
-        #                  '<em><a href="https://timsong-cpp.github.io/cppwp/n4659/foo.bar.baz#16">[foo.bar.baz]\xa73.4\xb616</a></em>',
-        #                   standard_ref('[foo.bar.baz]§3.4¶1, [foo.bar.baz]§3.4¶16'))
-        # self.assertEqual('<em><a href="https://timsong-cpp.github.io/cppwp/n4659/foo.bar.baz#16">[foo.bar.baz]\xa73.4\xb616</a></em>, '
-        #                  '<em><a href="https://timsong-cpp.github.io/cppwp/n4659/foo.bar.baz#1">[foo.bar.baz]\xa73.4\xb61</a></em>',
-        #                   standard_ref('[foo.bar.baz]§3.4¶16, [foo.bar.baz]§3.4¶1'))
-        # self.assertEqual('<em><a href="https://timsong-cpp.github.io/cppwp/n4659/foo.bar.baz#16">[foo.bar.baz]\xa73.4\xb616</a></em>, '
-        #                  '<em><a href="https://timsong-cpp.github.io/cppwp/n4659/foo.bar.baz#16">[foo.bar.baz]\xa73.4\xb616</a></em>',
-        #                   standard_ref('[foo.bar.baz]§3.4¶16, [foo.bar.baz]§3.4¶16'))
+        self.assertEqual('<em><a href="https://timsong-cpp.github.io/cppwp/n4659/foo.bar.baz#1">[foo.bar.baz]\xa73.4\xb61</a></em>, '
+                         '<em><a href="https://timsong-cpp.github.io/cppwp/n4659/foo.bar.baz#16">[foo.bar.baz]\xa73.4\xb616</a></em>',
+                          standard_ref('[foo.bar.baz]§3.4¶1, [foo.bar.baz]§3.4¶16'))
+        self.assertEqual('<em><a href="https://timsong-cpp.github.io/cppwp/n4659/foo.bar.baz#16">[foo.bar.baz]\xa73.4\xb616</a></em>, '
+                         '<em><a href="https://timsong-cpp.github.io/cppwp/n4659/foo.bar.baz#1">[foo.bar.baz]\xa73.4\xb61</a></em>',
+                          standard_ref('[foo.bar.baz]§3.4¶16, [foo.bar.baz]§3.4¶1'))
+        self.assertEqual('<em><a href="https://timsong-cpp.github.io/cppwp/n4659/foo.bar.baz#16">[foo.bar.baz]\xa73.4\xb616</a></em>, '
+                         '<em><a href="https://timsong-cpp.github.io/cppwp/n4659/foo.bar.baz#16">[foo.bar.baz]\xa73.4\xb616</a></em>',
+                          standard_ref('[foo.bar.baz]§3.4¶16, [foo.bar.baz]§3.4¶16'))
