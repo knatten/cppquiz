2878e660c0bb64a2a62d0e9f31c034bfadb50664
Anders Schau Knatten <anders@knatten.org>
2020-12-09 00:55:48 -0800

    Deploy script for the new host

---
+++ deploy.sh
@@ -0,0 +1,29 @@
+function header()
+{
+    echo
+    echo ------------------------------
+    echo $1
+    echo ------------------------------
+}
+source ../venv/bin/activate || exit $?
+header "Versions"
+echo -n "python: "
+which python || exit $?
+python --version || exit $?
+echo -n "Django version: "
+python -c "import django; print(django.__version__)" || exit $?
+
+header "Upgrading pip packages"
+pip install -r requirements.frozen.txt || exit $?
+echo "Django version is now: "
+python -c "import django; print(django.__version__)" || exit $?
+
+header "Migrating"
+python manage.py migrate || exit $?
+
+header "Collecting static"
+python manage.py collectstatic --noinput || exit $?
+
+header "Restarting"
+mkdir -p ../tmp || exit $?
+touch ../tmp/restart.txt || exit $?
