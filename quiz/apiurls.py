640ec5f8778deeee05e678b58670ea9f904351f6
PatriotRossii <patriotrossii2019@mail.ru>
2023-03-08 11:14:51 +0300

    Replace url with re_path in urlpatterns
    RemovedInDjango40Warning: django.conf.urls.url() is deprecated in favor of django.urls.re_path()

--- quiz/apiurls.py
+++ quiz/apiurls.py
@@ -1,7 +1,7 @@
-from django.conf.urls import *
+from django.conf.urls import re_path
 
 from quiz import api
 
 urlpatterns = [
-    url(r'^quiz', api.quiz, name='quiz'),
+    re_path(r'^quiz', api.quiz, name='quiz'),
 ]
