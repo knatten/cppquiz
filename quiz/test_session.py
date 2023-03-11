333874ce1444937821093a0e80e97682884dbd37
tocic <tocic@protonmail.ch>
2023-03-11 09:16:39 +0300

    Extract creating session with answers in test_session.py
    Signed-off-by: tocic <tocic@protonmail.ch>

--- quiz/test_session.py
+++ quiz/test_session.py
@@ -5,54 +5,55 @@ from quiz.game_data import *
 from quiz.models import *
 
 
+def create_session_with_answers_to(questions):
+    session = {}
+    user_data = UserData(session)
+
+    for question in questions:
+        user_data.register_correct_answer(question.pk)
+
+    return user_data
+
+
 class UserDataTest(unittest.TestCase):
 
     def test_given_new_session__has_no_correct_answers(self):
-        session = {}
-        s = UserData(session)
-        self.assertSetEqual(set(), s.get_correctly_answered_questions())
+        user_data = create_session_with_answers_to(tuple())
+        self.assertSetEqual(set(), user_data.get_correctly_answered_questions())
 
     def test_given_new_session__after_a_correct_answer_to_a_published_question_is_registered__it_is_listed_as_answered(self):
         q1 = Question.objects.create(state='PUB', difficulty=1)
         q2 = Question.objects.create(state='PUB', difficulty=1)
-        session = {}
-        s = UserData(session)
-        s.register_correct_answer(q1.pk)
-        self.assertSetEqual({q1.pk}, s.get_correctly_answered_questions())
-        s.register_correct_answer(q2.pk)
-        self.assertSetEqual({q1.pk, q2.pk}, s.get_correctly_answered_questions())
+        user_data = create_session_with_answers_to((q1,))
+        self.assertSetEqual({q1.pk}, user_data.get_correctly_answered_questions())
+        user_data.register_correct_answer(q2.pk)
+        self.assertSetEqual({q1.pk, q2.pk}, user_data.get_correctly_answered_questions())
 
     def test_given_new_session__after_a_correct_answer_to_a_retracted_question_is_registered__it_is_not_listed_as_answered(self):
         q = Question.objects.create(state='RET', difficulty=1)
-        session = {}
-        s = UserData(session)
-        s.register_correct_answer(q.pk)
-        self.assertSetEqual(set(), s.get_correctly_answered_questions())
+        user_data = create_session_with_answers_to((q,))
+        self.assertSetEqual(set(), user_data.get_correctly_answered_questions())
 
     def test_given_new_session__after_a_published_question_becomes_retracted__its_answer_is_not_listed_as_answered(self):
         q = Question.objects.create(state='PUB', difficulty=1)
-        session = {}
-        s = UserData(session)
-        s.register_correct_answer(q.pk)
-        self.assertSetEqual({q.pk}, s.get_correctly_answered_questions())
+        user_data = create_session_with_answers_to((q,))
+        self.assertSetEqual({q.pk}, user_data.get_correctly_answered_questions())
         q.state = 'RET'
         q.save()
-        self.assertSetEqual(set(), s.get_correctly_answered_questions())
+        self.assertSetEqual(set(), user_data.get_correctly_answered_questions())
 
     def test_given_existing_session__correct_answers_are_still_there(self):
         q = Question.objects.create(state='PUB', difficulty=1)
-        old_data = UserData({})
-        old_data.register_correct_answer(q.pk)
+        old_data = create_session_with_answers_to((q,))
         session = {'user_data': old_data}
         new_data = UserData(session)
         self.assertSetEqual({q.pk}, new_data.get_correctly_answered_questions())
 
     def test_clear_correct_answers(self):
         q = Question.objects.create(state='PUB', difficulty=1)
-        s = UserData({})
-        s.register_correct_answer(q.pk)
-        s.clear_correct_answers()
-        self.assertSetEqual(set(), s.get_correctly_answered_questions())
+        user_data = create_session_with_answers_to((q,))
+        user_data.clear_correct_answers()
+        self.assertSetEqual(set(), user_data.get_correctly_answered_questions())
 
 
 class save_user_dataTest(unittest.TestCase):
@@ -65,8 +66,7 @@ class save_user_dataTest(unittest.TestCase):
     def test_sets_user_data(self):
         q = Question.objects.create(state='PUB', difficulty=1)
         session = mock.MagicMock()
-        user_data = UserData({})
-        user_data.register_correct_answer(q.pk)
+        user_data = create_session_with_answers_to((q,))
         save_user_data(user_data, session)
         session.__setitem__.assert_called_once_with('user_data', user_data)
 
