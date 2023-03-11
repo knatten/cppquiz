fa0c1488a8dce5b8394811314afec36be6cd58b6
tocic <tocic@protonmail.ch>
2023-03-11 10:12:52 +0300

    Generalize create_questions() helper
    Signed-off-by: tocic <tocic@protonmail.ch>

--- quiz/integration_tests/test_training.py
+++ quiz/integration_tests/test_training.py
@@ -9,51 +9,51 @@ from quiz.test_helpers import create_questions
 
 class TrainingIntegrationTest(TestCase):
     def test_when_viewing_an_unpublished_question__gets_404(self):
-        question = self.create_question(False)
+        [question] = create_questions(1, state='NEW')
         response = self.client.get(reverse('quiz:question', kwargs={'question_id': question.pk}))
         self.assertEqual(404, response.status_code)
 
     def test_when_viewing_a_published_question__gets_it(self):
-        question = self.create_question(True)
+        [question] = create_questions(1, state='PUB', question='fluppa')
         response = self.client.get(reverse('quiz:question', kwargs={'question_id': question.pk}))
         self.assertContains(response, 'fluppa')
 
     def test_when_viewing_an_unpublished_question_with_a_preview_key__gets_it(self):
-        question = self.create_question(False, 'abc123')
+        [question] = create_questions(1, state='NEW', question='fluppa', preview_key='abc123')
         response = self.client.get(reverse('quiz:question', kwargs={'question_id': question.pk}),
                                    {'preview_key': 'abc123'})
         self.assertContains(response, 'fluppa')
 
     def test_when_viewing_a_published_question_with_a_preview_key__gets_it(self):
-        question = self.create_question(True, 'abc123')
+        [question] = create_questions(1, state='PUB', question='fluppa', preview_key='abc123')
         response = self.client.get(reverse('quiz:question', kwargs={'question_id': question.pk}),
                                    {'preview_key': 'abc123'})
         self.assertContains(response, 'fluppa')
 
     def test_when_viewing_an_unpublished_question_with_a_wrong_preview_key__gets_404(self):
-        question = self.create_question(False, 'abc123')
+        [question] = create_questions(1, state='NEW', preview_key='abc123')
         response = self.client.get("/quiz/question/%d?preview_key=%s" % (question.pk, 'wrong'))
         self.assertEqual(404, response.status_code)
 
     def test_when_viewing_an_unpublished_question_with_an_empty_preview_key__gets_404(self):
-        question = self.create_question(False, '')
+        [question] = create_questions(1, state='NEW', preview_key='')
         response = self.client.get("/quiz/question/%d?preview_key=%s" % (question.pk, ''))
         self.assertEqual(404, response.status_code)
 
     def test_when_viewing_an_unanswered_question__is_told_that_attempts_are_needed_before_giving_up_is_allowed(self):
-        question = self.create_question(True)
+        [question] = create_questions(1)
         response = self.client.get(reverse('quiz:question', kwargs={'question_id': question.pk}))
         self.assertContains(response, 'make 3 more attempts first')
 
     def test_when_viewing_a_correctly_answered_question__is_not_told_about_giving_up(self):
-        question = self.create_question(True)
+        [question] = create_questions(1)
         response = self.client.get(reverse('quiz:question', kwargs={'question_id': question.pk}),
                                    {'did_answer': 'answer', 'result': question.result, 'answer': question.answer})
         self.assertContains(response, 'Correct')
         self.assertNotContains(response, 'more attempts first')
 
     def test_when_viewing_an_incorrectly_answered_question__is_told_that_more_attempts_are_needed_before_giving_up_is_allowed(self):
-        question = self.create_question(True)
+        [question] = create_questions(1)
         response = self.client.get(reverse('quiz:question', kwargs={'question_id': question.pk}))
         self.assertContains(response, 'make 3 more attempts first')
         response = self.answer_question_incorrectly(question)
@@ -64,29 +64,22 @@ class TrainingIntegrationTest(TestCase):
         self.assertNotContains(response, 'more attempts first')
 
     def test_when_less_than_three_attempts_were_made__going_directly_to_the_giveup_page_is_not_allowed(self):
-        question = self.create_question(True)
+        [question] = create_questions(1)
         response = self.client.get(reverse('quiz:giveup', kwargs={'question_id': question.pk}))
         self.assertEqual(404, response.status_code)
 
     def test_when_viewing_a_retracted_question__is_warned(self):
-        question = self.create_question(True)
-        question.state = 'RET'
-        question.retraction_message = 'A very good reason'
-        question.save()
+        [question] = create_questions(1, state='RET', retraction_message='A very good reason')
         response = self.client.get(reverse('quiz:question', kwargs={'question_id': question.pk}))
         self.assertContains(response, 'This question has been retracted')
         self.assertContains(response, 'A very good reason')
 
     def test_when_viewing_a_question__it_gets_timestamped(self):
-        question = self.create_question(True)
+        [question] = create_questions(1)
         self.assertIsNone(question.last_viewed)
         response = self.client.get(reverse('quiz:question', kwargs={'question_id': question.pk}))
         self.assertEqual(200, response.status_code)
         self.assertLess((timezone.now() - Question.objects.get(pk=question.pk).last_viewed).total_seconds(), 10)
-
-    def create_question(self, published, preview_key=''):
-        return Question.objects.create(state='PUB' if published else 'NEW', question='fluppa', answer='buppa',
-                                       result='OK', hint='jotta', difficulty=1, preview_key=preview_key)
 
     def answer_question_incorrectly(self, question):
         return self.client.get(reverse('quiz:question', kwargs={'question_id': question.pk}),
--- quiz/test_session.py
+++ quiz/test_session.py
@@ -2,7 +2,7 @@ import mock
 import unittest
 
 from quiz.game_data import *
-from quiz.models import *
+from quiz.test_helpers import *
 
 
 def create_session_with_answers_to(questions):
@@ -22,20 +22,19 @@ class UserDataTest(unittest.TestCase):
         self.assertSetEqual(set(), user_data.get_correctly_answered_questions())
 
     def test_given_new_session__after_a_correct_answer_to_a_published_question_is_registered__it_is_listed_as_answered(self):
-        q1 = Question.objects.create(state='PUB', difficulty=1)
-        q2 = Question.objects.create(state='PUB', difficulty=1)
+        [q1, q2] = create_questions(2, state='PUB')
         user_data = create_session_with_answers_to((q1,))
         self.assertSetEqual({q1.pk}, user_data.get_correctly_answered_questions())
         user_data.register_correct_answer(q2.pk)
         self.assertSetEqual({q1.pk, q2.pk}, user_data.get_correctly_answered_questions())
 
     def test_given_new_session__after_a_correct_answer_to_a_retracted_question_is_registered__it_is_not_listed_as_answered(self):
-        q = Question.objects.create(state='RET', difficulty=1)
+        [q] = create_questions(1, state='RET')
         user_data = create_session_with_answers_to((q,))
         self.assertSetEqual(set(), user_data.get_correctly_answered_questions())
 
     def test_given_new_session__after_a_published_question_becomes_retracted__its_answer_is_not_listed_as_answered(self):
-        q = Question.objects.create(state='PUB', difficulty=1)
+        [q] = create_questions(1, state='PUB')
         user_data = create_session_with_answers_to((q,))
         self.assertSetEqual({q.pk}, user_data.get_correctly_answered_questions())
         q.state = 'RET'
@@ -43,14 +42,14 @@ class UserDataTest(unittest.TestCase):
         self.assertSetEqual(set(), user_data.get_correctly_answered_questions())
 
     def test_given_existing_session__correct_answers_are_still_there(self):
-        q = Question.objects.create(state='PUB', difficulty=1)
+        [q] = create_questions(1)
         old_data = create_session_with_answers_to((q,))
         session = {'user_data': old_data}
         new_data = UserData(session)
         self.assertSetEqual({q.pk}, new_data.get_correctly_answered_questions())
 
     def test_clear_correct_answers(self):
-        q = Question.objects.create(state='PUB', difficulty=1)
+        [q] = create_questions(1)
         user_data = create_session_with_answers_to((q,))
         self.assertSetEqual({q.pk}, user_data.get_correctly_answered_questions())
         user_data.clear_correct_answers()
@@ -65,7 +64,7 @@ class save_user_dataTest(unittest.TestCase):
         self.assertTrue(session.modified)
 
     def test_sets_user_data(self):
-        q = Question.objects.create(state='PUB', difficulty=1)
+        [q] = create_questions(1)
         session = mock.MagicMock()
         user_data = create_session_with_answers_to((q,))
         save_user_data(user_data, session)
