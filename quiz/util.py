c6b00251c328d24cc60ab541a20f106064d7c3d2
Anders Schau Knatten <anders@knatten.org>
2023-03-21 13:20:34 +0100

    Delete dead code
    Should have been removed in 08e92c8

--- quiz/util.py
+++ quiz/util.py
@@ -1,15 +1,5 @@
 from quiz.models import Question
 
 
-# http://stackoverflow.com/a/4581997
-def get_client_ip(request):
-    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
-    if x_forwarded_for:
-        ip = x_forwarded_for.split(',')[0]
-    else:
-        ip = request.META.get('REMOTE_ADDR')
-    return ip
-
-
 def get_published_questions():
     return Question.objects.filter(state='PUB')
